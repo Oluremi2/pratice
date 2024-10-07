b6cc0cd7aedae9e03ba032146472d7b89560638e
Oluremi2 <oluremimaryobakoyowa@gmail.com>
2024-10-07 08:37:43 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+
+// document.body.innerHTML="This is a content";
+
+//ADDITION
+
+var addTEXT= "Adding 20 and 40"
+
+
+document.write(addTEXT)
+document.write("<br>")
+
+var addX = 20,
+    addY = 40;
+
+document.write(addX + addY)
+document.write("<br>")
+    
+ 
+
+//SUBTRACTION
+
+document.write("<br>")
+var subtractText= "subtract 10 and 30"
+
+
+document.write(subtractText)
+document.write("<br>")
+
+var subtractX = 30,
+    subtractY = 10;
+
+document.write(addX - addY)
+document.write("<br>")
+
+
+
+//MULTIPLICATION
+
+document.write("<br>")
+var multiplyText= "multiply 2 by 5"
+
+
+document.write(multiplyText)
+document.write("<br>")
+
+var multiplyX = 2,
+    multiplyY = 5;
+
+document.write(multiplyX * multiplyY)
+document.write("<br>")
+
+
+//EXPONENTIAL
+
+document.write("<br>")
+var exponentText= "exponent 6 by 5"
+
+
+document.write(exponentText)
+document.write("<br>")
+
+var exponentX = 2,
+    exponentY = 5;
+
+document.write(exponentX ** exponentY)
+document.write("<br>")
+    
